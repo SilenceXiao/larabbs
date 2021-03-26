287695595d9538ded1e413a2abbc0162831d24e6
rabbit <1282288696@qq.com>
2021-03-26 11:42:04 +0800

    test

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -34,6 +34,7 @@ class UsersController extends Controller
         if($request->avatar){
             $result = $images->save($request->avatar,'avatar',$user->id,416);
             if($result){
+                
                 $data['avatar'] = $result['path'];
             }
         }
