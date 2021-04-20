75b7d4b5fb15e43d6d8944d743f718446796c792
rabbit <1282288696@qq.com>
2021-04-20 18:16:13 +0800

    消息通知-通知列表

--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -1,11 +1,33 @@
 @extends('layouts.app')
 @section('title','消息通知')
 @section('content')
-    @if($notifications->count())
-        @foreach($notifications as $notification)
-        <a href="{{ $notification->data['topic_link'] }}">{{ $notification->data['topic_title'] }}</a>
-        @endforeach
-    @else
-        <div class="empty-block"> 没有消息通知</div>
-    @endif
+<div class="container">
+    <div class="col-md-10 offset-md-1">
+      <div class="card ">
+
+        <div class="card-body">
+
+          <h3 class="text-xs-center">
+            <i class="far fa-bell" aria-hidden="true"></i> 我的通知
+          </h3>
+          <hr>
+
+          @if ($notifications->count())
+
+            <div class="list-unstyled notification-list">
+              @foreach ($notifications as $notification)
+                @include('notifications.types._' . Str::snake(class_basename($notification->type)))
+              @endforeach
+
+              {!! $notifications->render() !!}
+            </div>
+
+          @else
+            <div class="empty-block">没有消息通知！</div>
+          @endif
+
+        </div>
+      </div>
+    </div>
+</div>
 @stop
